41ed832dd178da8dab95b2747ad0201657ddf49d
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-25 23:13:21 +0100

    Support piping

--- cli.js
+++ cli.js
@@ -8,15 +8,27 @@ var path = require('path')
 var styl = require('stylus')
 var rpl  = require('replace-require')
 var esc  = require('js-string-escape')
-var src  = fs.readFileSync(args['_'][0],'utf-8')
-var dir  = path.dirname(args['_'][0])
 
-var _src = rpl(src, function(target) {
-    if (target.indexOf('.styl') >= 0) {
-        var _file  = target.slice(9,-2)
-        var _style = fs.readFileSync(path.resolve(dir,_file),'utf-8')
-        return '"'+esc(styl(_style).render())+'"'
-    }
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data', function(data) {
+    replaceAndDie(data,'.')
 })
 
-process.stdout.write(_src)
+if (args['_'].length > 0) {
+    var src  = fs.readFileSync(args['_'][0],'utf-8')
+    var dir  = path.dirname(args['_'][0])
+    replaceAndDie(src,dir)
+} 
+
+function replaceAndDie(src, dir) {
+    var _src = rpl(src, function(target) {
+        if (target.indexOf('.styl') >= 0) {
+            var _file  = target.slice(9,-2)
+            var _style = fs.readFileSync(path.resolve(dir,_file),'utf-8')
+            return '"'+esc(styl(_style).render())+'"'
+        }
+    })
+    process.stdout.write(_src)
+    process.exit(0)
+}
