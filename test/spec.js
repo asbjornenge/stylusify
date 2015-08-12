1ee83967978358d5b4ae809eb1a6e826a71e4d23
Joe Zimmerman <contact@joezimjs.com>
2015-08-12 12:33:41 -0500

    Fixed test
    `style.indexOf('body {  background-color: #ffc0cb;}' == 0)` is like saying `style.indexOf(false)` and returns -1, which is truthy, so no matter what value `style` has, this assertion would have passed. I adjusted the position of the end parenthesis so that it's actually checking the index of the string. :)

--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var style  = require('./style.styl')
 
 it('allows you to require stylus files', function() {
-    assert(style.indexOf('body {  background-color: #ffc0cb;}' == 0))
+    assert(style.indexOf('body {  background-color: #ffc0cb;}') == 0)
 })
 
 it('correctly escapes css string', function() {
