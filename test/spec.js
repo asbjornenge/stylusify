91502e1fd6445785b1330c287c9859e802f7534e
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-08-28 13:40:05 +0200

    Fixed some failing test

--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,8 @@ var assert = require('assert')
 var style  = require('./style.styl')
 
 it('allows you to require stylus files', function() {
-    assert(style.indexOf('body {  background-color: #ffc0cb;}') == 0)
+    assert(style.indexOf('body {') == 0)
+    assert(style.indexOf('background-color: #ffc0cb') > 0)
 })
 
 it('correctly escapes css string', function() {
